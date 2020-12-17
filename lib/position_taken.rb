9b502211154e14b1f9e3c9fc63b4dc3c57cb6263
Kiana Long <kianajade42.kl@gmail.com>
2020-12-17 02:46:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -9,7 +9,7 @@ def position_taken? (board, index)
  def valid_move?(board, index)
    if !position_taken?(board, index) && (index).between?(0,8)
      return true
-   else 
+   else
      return false
    end
  end
